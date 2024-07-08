9e629e1a5108eac46d93bf14ebe8db0ff8f9fb70
Sean-Tay <zenzizenzizenzic8888@gmail.com>
2024-07-08 18:08:37 +0800

    Incorporate http4k Starter Template.

--- server/src/main/kotlin/Main.kt
+++ server/src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
 package org.sean_tay.kotlin_react
 
+import org.http4k.server.SunHttp
+import org.http4k.server.asServer
+import org.sean_tay.kotlin_react.handlers.handlers
+
 fun main() {
-    println("Hello World!")
+    handlers().asServer(SunHttp(9000)).start()
 }
