ada9a01843c5aec2cbce4720c5e23e293b617fe2
Sean-Tay <taysiangmengsean@gmail.com>
2024-07-10 19:24:47 +0800

    Adjust vite Dev Server Config to proxy api calls.

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -7,6 +7,18 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
 
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:9000',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, 'http://localhost:9000/api')
+      },
+    }
+  },
+
   test: {
     typecheck: {
       tsconfig: './tsconfig.vitest.json'
