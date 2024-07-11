93fde671679654afae7a26ed5903df52a8e163d4
Sean-Tay <taysiangmengsean@gmail.com>
2024-07-11 18:19:26 +0800

    Add ui Transport Methods for interacting with server System API.

---
+++ ui/src/transports/TransportProvider.tsx
@@ -0,0 +1,30 @@
+import { useState, createContext, PropsWithChildren, useEffect } from 'react'
+import ServerSystemTransport from './ServerSystemTransport'
+
+type TransportContextState = {
+  initialized: boolean
+
+  serverSystemTransport: ServerSystemTransport
+}
+
+export const TransportContext = createContext<TransportContextState>({ initialized: false } as TransportContextState)
+
+export const TransportProvider = ({ children }: PropsWithChildren) => {
+  const [ transportContextState, setTransportContextState ] = useState<TransportContextState>({ initialized: false } as TransportContextState)
+
+  useEffect(() => {
+    setTransportContextState({
+      initialized: true,
+
+      serverSystemTransport: new ServerSystemTransport()
+    })
+  }, [])
+
+  return <TransportContext.Provider
+    value={transportContextState}
+  >
+    { transportContextState.initialized ? children : null }
+  </TransportContext.Provider>
+}
+
+export default TransportProvider
