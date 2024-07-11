d659eda6c7e298bded59d1d0b966614991ced02e
Sean-Tay <zenzizenzizenzic8888@gmail.com>
2024-07-11 19:52:48 +0800

    Add singlePageApp Route.

--- server/src/main/kotlin/handlers/handlers.kt
+++ server/src/main/kotlin/handlers/handlers.kt
@@ -1,11 +1,11 @@
 package org.sean_tay.kotlin_react.handlers
 
-import org.http4k.routing.RoutingHttpHandler
-import org.http4k.routing.bind
-import org.http4k.routing.routes
+import org.http4k.routing.*
 
 fun handlers(): RoutingHttpHandler {
     return routes(
         "/api" bind apiHandler(),
+
+        singlePageApp()
     )
 }
