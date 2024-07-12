9d3378a190d05b88474ab364c773702021faaa33
Sean-Tay <zenzizenzizenzic8888@gmail.com>
2024-07-12 20:45:27 +0800

    Add tasks to build.gradle.kts to handle ui and uberJar compilation.

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,5 +1,10 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     kotlin("jvm") version "2.0.0"
+
+    id("com.github.node-gradle.node") version "7.0.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "org.sean_tay.kotlin_react"
@@ -8,6 +13,8 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+
+// server
 
 dependencies {
     implementation(platform("org.http4k:http4k-bom:${findProperty("http4kVersion")}"))
@@ -29,4 +36,36 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(11)
+}
+
+// ui
+
+node {
+    download.set(true)
+    nodeProjectDir.set(project.file("../ui"))
+}
+
+extensions.findByName("buildScan")?.withGroovyBuilder {
+    setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
+    setProperty("termsOfServiceAgree", "yes")
+}
+
+tasks.register<Sync>("moveUiDist") {
+    from(project.file("../ui/dist"))
+    into(project.file("./src/main/resources/public"))
+}
+
+tasks.findByName("npm_run_build")?.dependsOn("npmInstall")
+tasks.findByName("moveUiDist")?.dependsOn("npm_run_build")
+
+tasks.create("buildUi").dependsOn("moveUiDist")
+
+// uberJar
+
+tasks.named<ShadowJar>("shadowJar") {
+    archiveBaseName.set("server")
+    mergeServiceFiles()
+    manifest {
+        attributes(mapOf("Main-Class" to "${rootProject.group}.MainKt"))
+    }
 }
